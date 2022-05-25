814277754ab82c234794c2014c8c6ddba3c10054
Denis Wirries <denis@wirries.me>
2022-05-25 11:33:09 +0200

    Add init

---
+++ src/main/java/me/wirries/wingetfx/Application.java
@@ -0,0 +1,17 @@
+package me.wirries.wingetfx;
+
+/**
+ * The main class for starting the JavaFX application
+ * and showing the main stage.
+ *
+ * @author denisw
+ * @version 1.0
+ * @since 25.05.2022
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
